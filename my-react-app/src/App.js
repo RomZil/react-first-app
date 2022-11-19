46fe089a37fd9aef92ec34834d11087cb0d27902
RomZil <zilber.rom@gmail.com>
2022-11-19 15:31:25 +0200

    new try

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,24 +1,40 @@
-import { Navigate, useRoutes } from 'react-router-dom';
-import ContextFilter from './components/Context/ContextFilter';
-import ContextProvider from './components/Context/ContextProvider';
-import Details from './pages/Details/Details';
-import Header from './layout/Header/Header';
-import Products from './pages/Products/Products';
+
+import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
-  let router = useRoutes([
-    { path: '/', element: <Products /> },
-    { path: '/:id', element: <Details /> },
-    { path: '*', element: <Navigate to={'/'} /> },
-  ])
+
+  const expense = [
+    { title: 'Apple', amount: 5.90 , img: './image/Apple.jpg'},
+    { title: 'Milk', amount: 6.50, img: './image/Milk.jpg' },
+    { title: 'Cheese', amount: 12.00, img: './image/Cheese.jpg'},
+    { title: 'Meat', amount: 55.60 , img: './image/Meat.jpg'},
+
+  ];
   return (
-    <ContextProvider>
-      <ContextFilter>
-        <Header />
-        {router}
-      </ContextFilter>
-    </ContextProvider>
+    <div>
+      <h2> Products list</h2>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        img={expense[0].img}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        img={expense[1].img}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        img={expense[2].img}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        img={expense[3].img}
+      ></ExpenseItem>
+    </div>
   );
 }
 
