8a3f80870eace62a6d087eacbbe1f920e65a14e6
RomZil <zilber.rom@gmail.com>
2022-11-23 17:55:52 +0200

    fix - finish assigment no bounos

--- my-react-app/src/components/ExpenseItem.js
+++ my-react-app/src/components/ExpenseItem.js
@@ -5,8 +5,8 @@ function ExpenseItem(props) {
 
     return (
         <div className='expense-item'>
-            <h1 className="expense-item__description "> {props.title}{"\n"}</h1>
-            <h2 className="expense-item__price"> {props.amount} $ </h2>
+            <div className="expense-item__title "> {props.title}</div>
+            <div className="expense-item__price"> {props.amount} $ </div>
             <img className="expense-item__img" src={props.img} alt=""></img> 
         </div>
     );
