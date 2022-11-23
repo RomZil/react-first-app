0618ab019b51a138b0011b780c68107f89760271
RomZil <zilber.rom@gmail.com>
2022-11-23 18:11:51 +0200

    WTF

--- my-react-app/src/components/ExpenseItem.js
+++ my-react-app/src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
-
-import './ExpenseItem.css'
+import { Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
-    return (
-        <div className='expense-item'>
-            <div className="expense-item__title "> {props.title}</div>
-            <div className="expense-item__price"> {props.amount} $ </div>
-            <img className="expense-item__img" src={props.img} alt=""></img> 
-        </div>
-    );
+  return (
+    <Card className="text-center" style={{ width: "200px", height: "250px" }}  >
+      <Card.Header style={{ fontSize: "3rem" }}>{props.title}</Card.Header>
+      <Card.Img variant="top" src={props.img} style={{ width: "198px", height: "150px" }} />
+      <Card.Body>
+        <Card.Footer style={{ fontSize: "2rem", fontFamily: "cursive" }}>
+          {props.amount} $
+        </Card.Footer>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
