3eb0506b7e2d3abbfa82a19ee59d2b27a4e95fb4
Maya Betzalel <maya.betzalel@moovit.com>
2022-11-27 21:55:25 +0200

    Router to product extra detailes

--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -2,89 +2,45 @@ import ExpenseItem from "../components/ExpenseItem";
 import "./Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
-import appleimg from "../image/apple.jpg";
-import milkimg from "../image/milk.jpg";
-import chesseimg from "../image/chesse.jpg";
-import meatimg from "../image/meat.jpg";
-// import { Link } from "react-router-dom";
+import data from '../data/mockdata.js'
 
 function main() {
-  const expense = [
-    {
-      id: 0,
-      title: "Apple",
-      amount: 5.9,
-      img: appleimg,
-      sec_img: "",
-      store: "shokishok",
-      desc: "Big red apple",
-    },
-    {
-      id: 1,
-      title: "Milk",
-      amount: 6.5,
-      img: milkimg,
-      sec_img: "",
-      store: "shokishok",
-      desc: "5% Oat Milk",
-    },
-    {
-      id: 2,
-      title: "Cheese",
-      amount: 12.0,
-      img: chesseimg,
-      sec_img: "",
-      store: "shokishok",
-      desc: "American Cheese",
-    },
-    {
-      id: 3,
-      title: "Meat",
-      amount: 55.6,
-      img: meatimg,
-      sec_img: "",
-      store: "shokishok",
-      desc: "Fresh ground beef",
-    },
-  ];
-
   return (
     <div className="main">
       <div className="page_title"> Products list </div>
       <Container>
         <Row>
           <Col>
-            {/* <Link to="/ItemDetails/0"> */}
             <ExpenseItem
-              id={expense[0].id}
-              title={expense[0].title}
-              amount={expense[0].amount}
-              img={expense[0].img}
+              id={data[0].id}
+              title={data[0].title}
+              amount={data[0].amount}
+              img={data[0].img}
             ></ExpenseItem>
             {/* </Link> */}
           </Col>
           <Col>
             <ExpenseItem
-              id={expense[0].id}
-              title={expense[1].title}
-              amount={expense[1].amount}
-              img={expense[1].img}
+              id={data[1].id}
+              title={data[1].title}
+              amount={data[1].amount}
+              img={data[1].img}
             ></ExpenseItem>
           </Col>
           <Col>
             <ExpenseItem
-              id={expense[0].id}
-              title={expense[2].title}
-              amount={expense[2].amount}
-              img={expense[2].img}
+              id={data[2].id}
+              title={data[2].title}
+              amount={data[2].amount}
+              img={data[2].img}
             ></ExpenseItem>
           </Col>
           <Col>
             <ExpenseItem
-              id={expense[0].id}
-              title={expense[3].title}
-              amount={expense[3].amount}
-              img={expense[3].img}
+              id={data[3].id}
+              title={data[3].title}
+              amount={data[3].amount}
+              img={data[3].img}
             ></ExpenseItem>
           </Col>
         </Row>
